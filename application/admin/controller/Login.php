1faef792e4cc3422cc65768dda39e547e3c7b924
bishyan <bishiyan2008@163.com>
2021-09-14 15:04:12 +0800

    ..

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -6,6 +6,11 @@ class Login extends Common
 {
     public function login()
     {
+        echo md5('GG_shop1234abcd');
         return $this->fetch();
+    }
+    
+    public function checkout() {
+        echo md5('GG_shop1234abcd');
     }
 }
